17a2b2c94775dff289e58df803931e8d23c51f9e
Devashish <pundkardevashish@gmail.com>
2025-08-24 09:25:51 +0530

    made changes to resolve error in deployement

--- src/db/index.js
+++ src/db/index.js
@@ -3,10 +3,11 @@ import { DB_NAME } from "../constants.js";
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(`${process.env.URI_KEY}/${DB_NAME}`);
-    console.log(`MongoDB Connected:  ${conn.connection.host}`);
+    const conn = await mongoose.connect(process.env.URI_KEY);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+    console.log(`Database: ${conn.connection.name}`);
   } catch (error) { 
-    console.log("MONGODB connection error",error);
+    console.log("MONGODB connection error", error);
     process.exit(1);
     }
 }
